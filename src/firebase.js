eae77a4e1c91da23f718c11b4ee064679aa90d83
Rad <chihaby@gmail.com>
2019-10-23 14:33:50 -0700

    hides key

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const config = {
-    apiKey: "AIzaSyA3C2ip511tOPc1_DfVU9MNrlIpCeFbmWQ",
-    authDomain: "think-piece-live-dc187.firebaseapp.com",
-    databaseURL: "https://think-piece-live-dc187.firebaseio.com",
-    projectId: "think-piece-live-dc187",
-    storageBucket: "think-piece-live-dc187.appspot.com",
-    messagingSenderId: "741248823364",
-    appId: "1:741248823364:web:607654ee68ff0e6a608c11",
-    measurementId: "G-QW2K7QF1QG"
+// keys in txt file
 };
 
 firebase.initializeApp(config);
