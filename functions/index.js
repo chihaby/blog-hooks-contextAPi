de2fde3845c0449874248c2151be655e3307c693
Rad <chihaby@gmail.com>
2019-11-15 13:07:33 -0800

    adding deployment files

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+const firestore = admin.firestore();
+firestore.settings({ timestampsInSnapshots: true });
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!');
+});
+
+exports.getAllPosts = functions.https.onRequest(async (request, response) => {
+  const snapshot = await firestore.collection('posts').get();
+  const posts = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+  response.json({ posts });
+});
